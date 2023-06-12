1fd8efb36785b530b4f5f114eb5a9950d26fb1f7
Martin Helmich <m.helmich@mittwald.de>
2023-06-12 16:09:56 +0200

    Add <OperationHint> component and use that where possible

---
+++ src/components/OperationHint/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Admonition from "@theme/Admonition";
+import Translate from "@docusaurus/Translate";
+import OperationLink from "../OperationLink";
+
+export default function OperationHint({tag, operation}) {
+  return (
+    <Admonition type="info">
+      <Translate id={"components.OperationHint.text"}>For details please refer to the endpoint documentation for the following endpoint</Translate>:{" "}
+      <OperationLink tag={tag} operation={operation}>
+        <code>{operation}</code>
+      </OperationLink>
+    </Admonition>
+  );
+}
