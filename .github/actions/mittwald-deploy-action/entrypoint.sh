0a4567fa42c52b83c4df6fb0da74c0aaf1cb8dc7
Martin Helmich <m.helmich@mittwald.de>
2023-10-16 16:14:52 +0200

    Some more debugging

--- .github/actions/mittwald-deploy-action/entrypoint.sh
+++ .github/actions/mittwald-deploy-action/entrypoint.sh
@@ -16,7 +16,10 @@ fi
 cd /github/workspace
 
 if [ ! -f ./deploy.php ] ; then
+  echo "no deploy.php file found; using distributed default deploy.php..."
   cp /deploy.php .
+else
+  echo "using deploy.php from project root"
 fi
 
 mkdir -p .mw-deployer/
@@ -27,7 +30,7 @@ echo "${MITTWALD_SSH_PRIVATE_KEY}" > ./.mw-deployer/deployer
 chmod 0600 ./.mw-deployer/deployer
 
 echo "::group::Deploying application ${MITTWALD_APP_ID}"
-dep --ansi "${INPUT_VERBOSITY}" deploy \
+dep --ansi --no-interaction "${INPUT_VERBOSITY}" deploy \
   -o mittwald_app_id="${MITTWALD_APP_ID}" \
   -o mittwald_ssh_public_key_file=./.mw-deployer/deployer.pub \
   -o mittwald_ssh_private_key_file=./.mw-deployer/deployer \
