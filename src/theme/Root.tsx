4983790fcabd3af3503ac85947e7d5e417a64a24
Martin Helmich <m.helmich@mittwald.de>
2024-04-16 17:06:40 +0200

    Configure cookieless analytics with Matomo

---
+++ src/theme/Root.tsx
@@ -0,0 +1,34 @@
+import React, { PropsWithChildren, useEffect } from "react";
+import { createInstance, MatomoProvider, useMatomo } from "@datapunt/matomo-tracker-react";
+import { useLocation } from "@docusaurus/router";
+
+function PageViewTracker({ children }: PropsWithChildren<{}>) {
+  const { trackPageView } = useMatomo();
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log("tracking pageview", location.pathname);
+    trackPageView({});
+  }, [location]);
+
+  return children;
+}
+
+export default function Root({ children }: PropsWithChildren<{}>) {
+  const matomoInstance = createInstance({
+    urlBase: "https://developer.mittwald.de/stats/",
+    siteId: 1,
+    configurations: {
+      disableCookies: true
+      // setSecureCookie: true,
+    }
+  });
+
+  return (
+    <MatomoProvider value={matomoInstance}>
+      <PageViewTracker>
+        {children}
+      </PageViewTracker>
+    </MatomoProvider>
+  );
+}
