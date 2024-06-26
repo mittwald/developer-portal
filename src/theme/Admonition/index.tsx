47134c29b5820b80b54fb7f980e0090eecd8099b
Martin Helmich <m.helmich@mittwald.de>
2024-06-26 11:09:13 +0200

    Make admonitions use InlineAlert component

---
+++ src/theme/Admonition/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import type AdmonitionType from "@theme/Admonition";
+import type { WrapperProps } from "@docusaurus/types";
+import InlineAlert from "@mittwald/flow-react-components/InlineAlert";
+import Heading from "@mittwald/flow-react-components/Heading";
+import Content from "@mittwald/flow-react-components/Content";
+import { translate } from "@docusaurus/Translate";
+
+type Props = WrapperProps<typeof AdmonitionType>;
+
+export default function AdmonitionWrapper(props: Props): JSX.Element {
+  const status = {
+    note: "info",
+    caution: "warning",
+  };
+
+  return (
+    <InlineAlert status={status[props.type]}>
+      <Heading>
+        {props.title ??
+          translate({
+            id: `theme.admonition.${props.type}`,
+            message: props.type,
+          })}
+      </Heading>
+      <Content>{props.children}</Content>
+    </InlineAlert>
+  );
+
+  // return (
+  //   <>
+  //     <Admonition {...props} />
+  //   </>
+  // );
+}
