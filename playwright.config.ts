7ff0adb831298d3b3f0755980caeb7c08cbb7f7a
Lars Bergmann <L.Bergmann@mittwald.de>
2026-01-09 16:43:11 +0100

    Add playwright

---
+++ playwright.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  
+  // Run tests in files in parallel
+  fullyParallel: true,
+  
+  // Fail the build on CI if you accidentally left test.only in the source code
+  forbidOnly: !!process. env.CI,
+  
+  // Retry on CI only
+  retries: process.env.CI ? 2 : 0,
+  
+  // Opt out of parallel tests on CI
+  workers: process.env.CI ? 1 : undefined,
+  
+  // Reporter to use
+  reporter: 'html',
+  
+  use: {
+    // Base URL for tests
+    baseURL: 'http://localhost:3000',
+    
+    // Collect trace when retrying the failed test
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  // Run your local dev server before starting the tests
+  webServer: {
+    command: 'npm run serve',
+    url: 'http://localhost:3000',
+    reuseExistingServer: ! process.env.CI,
+    timeout: 120 * 1000, // 2 minutes for build
+  },
+});
