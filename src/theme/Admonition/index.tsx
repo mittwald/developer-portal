d900cfacd749e283a48dcfee44777fff28f04329
Lars Bergmann <L.Bergmann@mittwald.de>
2026-08-18 11:57:36 +0200

    Preliminary fix for overflow of codeboxes in notes, warnings etc

--- src/theme/Admonition/index.tsx
+++ src/theme/Admonition/index.tsx
@@ -1,36 +1,10 @@
 import React from "react";
 import type AdmonitionType from "@theme/Admonition";
+import OriginalAdmonition from "@theme-original/Admonition";
 import type { WrapperProps } from "@docusaurus/types";
-import { Alert } from "@mittwald/flow-react-components";
-import { Heading } from "@mittwald/flow-react-components";
-import { Content } from "@mittwald/flow-react-components";
-import { translate } from "@docusaurus/Translate";
 
 type Props = WrapperProps<typeof AdmonitionType>;
 
 export default function AdmonitionWrapper(props: Props): JSX.Element {
-  const status = {
-    note: "info",
-    caution: "warning",
-    warning: "warning",
-  };
-
-  return (
-    <Alert status={status[props.type]}>
-      <Heading>
-        {props.title ??
-          translate({
-            id: `theme.admonition.${props.type}`,
-            message: props.type,
-          })}
-      </Heading>
-      <Content>{props.children}</Content>
-    </Alert>
-  );
-
-  // return (
-  //   <>
-  //     <Admonition {...props} />
-  //   </>
-  // );
+  return <OriginalAdmonition {...props} />;
 }
