c33d1f820e49d227346c1384176ea54aa21af2bc
Lars Bergmann <L.Bergmann@mittwald.de>
2026-08-18 12:19:18 +0200

    better fix flow components

--- src/theme/Admonition/index.tsx
+++ src/theme/Admonition/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import type AdmonitionType from "@theme/Admonition";
-import OriginalAdmonition from "@theme-original/Admonition";
 import type { WrapperProps } from "@docusaurus/types";
+import { Alert } from "@mittwald/flow-react-components";
+import { Heading } from "@mittwald/flow-react-components";
+import { Content } from "@mittwald/flow-react-components";
+import { translate } from "@docusaurus/Translate";
 
 type Props = WrapperProps<typeof AdmonitionType>;
 
-export default function AdmonitionWrapper(props: Props): JSX.Element {
-  return <OriginalAdmonition {...props} />;
+export default function AdmonitionWrapper(props: Props): React.JSX.Element {
+  const status =
+    props.type === "warning" || props.type === "caution" ? "warning" : "info";
+
+  return (
+    <Alert status={status}>
+      <Heading>
+        {props.title ??
+          translate({
+            id: `theme.admonition.${props.type}`,
+            message: props.type,
+          })}
+      </Heading>
+      <Content>{props.children}</Content>
+    </Alert>
+  );
 }
