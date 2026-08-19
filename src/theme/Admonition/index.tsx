6b37b3d07ddd03b5e9c7dbd170b3c93c1688c37d
Lars Bergmann <L.Bergmann@mittwald.de>
2026-08-19 10:23:34 +0200

    Fix flow wrapper, add overflow for codeboxes within alerts

--- src/theme/Admonition/index.tsx
+++ src/theme/Admonition/index.tsx
@@ -8,15 +8,12 @@ import { translate } from "@docusaurus/Translate";
 
 type Props = WrapperProps<typeof AdmonitionType>;
 
-export default function AdmonitionWrapper(props: Props): JSX.Element {
-  const status = {
-    note: "info",
-    caution: "warning",
-    warning: "warning",
-  };
+export default function AdmonitionWrapper(props: Props): React.JSX.Element {
+  const status =
+    props.type === "warning" || props.type === "caution" ? "warning" : "info";
 
   return (
-    <Alert status={status[props.type]}>
+    <Alert status={status}>
       <Heading>
         {props.title ??
           translate({
@@ -27,10 +24,4 @@ export default function AdmonitionWrapper(props: Props): JSX.Element {
       <Content>{props.children}</Content>
     </Alert>
   );
-
-  // return (
-  //   <>
-  //     <Admonition {...props} />
-  //   </>
-  // );
 }
